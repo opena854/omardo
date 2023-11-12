c7c2bb1342af0e26fd2e2e2692e2ef02b0f87f8f
Omar Peña <opena854@gmail.com>
2023-11-12 17:44:34 -0400

    preparing for production environment

---
+++ omardo/omardo/settings/development.py
@@ -0,0 +1,15 @@
+from .base import *
+
+SECRET_KEY = "django-secret-key"
+
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
---
+++ omardo/omardo/settings/producttion.py
@@ -0,0 +1,28 @@
+from .base import *
+import os
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+ALLOWED_HOSTS = ["*"]
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": "postgres",
+        "PORT": "5432",
+    }
+}
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
