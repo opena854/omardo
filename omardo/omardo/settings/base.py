9b3a39c382bd1b33027f09f93779f99c7532de91
Omar Peña <opena854@gmail.com>
2023-11-14 02:46:29 -0400

    main app added

--- omardo/omardo/settings/base.py
+++ omardo/omardo/settings/base.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -26,6 +26,7 @@ BASE_APPS = [
 
 LOCAL_APPS = [
     "polls.apps.PollsConfig",
+    "main.apps.MainConfig",
 ]
 
 THIRDS_APPS = []
--- omardo/omardo/urls.py
+++ omardo/omardo/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path("", include("main.urls")),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
