08db6d3429ec758812cf20393db1651cc008be8f
Omar Peña <opena854@gmail.com>
2023-11-14 19:10:27 -0400

    config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+export DJANGO_SETTINGS_MODULE=omardo.settings.production
+
 echo 'Running collecstatic...'
-python manage.py collectstatic --no-input --settings=config.settings.production
+python omardo/manage.py collectstatic --no-input
 
 echo 'Applying migrations...'
-python omardo/manage.py migrate --settings=config.settings.production
-
+python omardo/manage.py migrate
 
 echo 'Running server...'
-python omardo/manage.py runserver 0.0.0.0:8000 --settings=config.settings.production
+python omardo/manage.py runserver 0.0.0.0:8000
 
 #gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:$PORT
