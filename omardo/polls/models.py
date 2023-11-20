2b0333fef1e43e2717bda2bae87bac692f6199d9
Omar Peña <opena854@gmail.com>
2023-11-19 22:30:57 -0400

    poll porcentaje de votos

--- omardo/polls/models.py
+++ omardo/polls/models.py
@@ -33,5 +33,9 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    # def votes_rate(self):
+    #     total = self.question.votes_sum()
+    #     return self.votes / total
+
     def __str__(self):
         return self.choice_text
